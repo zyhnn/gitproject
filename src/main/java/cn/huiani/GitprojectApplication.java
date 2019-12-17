bb212048eebdcbb206988321cc28a4bcd2b13a0b
zyh <zyh@163.com>
2019-12-17 20:24:06 +0800

    sout

--- src/main/java/cn/huiani/GitprojectApplication.java
+++ src/main/java/cn/huiani/GitprojectApplication.java
@@ -8,6 +8,7 @@ public class GitprojectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitprojectApplication.class, args);
+        System.out.println();
     }
 
 }
