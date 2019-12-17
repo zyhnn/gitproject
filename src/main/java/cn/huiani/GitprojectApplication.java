566da80608e8c8b7bdf9698a2012ecc96c00d18d
zyh <zyh@163.com>
2019-12-17 20:23:17 +0800

    init

---
+++ src/main/java/cn/huiani/GitprojectApplication.java
@@ -0,0 +1,13 @@
+package cn.huiani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/huiani/GitprojectApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.huiani;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
